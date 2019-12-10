0c1d3367d9045e2453de5ba0b1741520823b20dd
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2019-12-10 09:08:34 +0900

    added Reservation model

---
+++ reserver/models.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+
+from accounts.models import User
+
+
+class Reservation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    start_at=models.DateTimeField(auto_now=False)
+    end_at = models.DateTimeField(auto_now=False)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='予約')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
